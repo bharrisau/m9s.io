c9d0a014da6a8d34668aecb2faa5d521f89b82c3
Ben Harris <mail@bharr.is>
2018-06-17 09:05:37 +0300

    Happy with the post loader now

--- src/Post.js
+++ src/Post.js
@@ -1,55 +1,52 @@
 import { createElement } from 'inferno-create-element';
 import 'highlight.js/styles/default.css';
-//import format from 'date-fns/format';
+import format from 'date-fns/format';
 
 const posts = require.context('!./post-loader!./_posts', false, /.md$/);
 
-//
-//const styles = {
-//    title: 'f2 lh-title',
-//    date: 'f6',
-//    p: 'f5 lh-copy',
-//};
-//
-//function toPost({ stub, short }) {
-//    this.Compiler = compile;
-//
-//    function toVNode(node) {
-//        var style = styles[node.tagName];
-//
-//        if (style) {
-//            node.properties.class = style;
-//        }
-//
-//        return toH(createElement, node);
-//    }
-//
-//    function compile(node) {
-//        var match = stub.match(/.\/(\d+-\d+-\d+)-([^.]+).*/);
-//        var date = match[1];
-//        var tag = match[2];
-//
-//        var yamlNode = node.children.find(c => c.type === 'yaml') || {};
-//        var header = yaml.safeLoad(yamlNode.value, 'utf8') || {};
-//
-//        var content = node
-//            .children
-//            .filter(n => n.type !== 'text')
-//            .slice(0, short ? 4 : undefined)
-//            .map(toVNode);
-//
-//	      return (
-//                <article id={date + '-' + tag}>
-//                <time class={styles.date} datetime={date}>{format(date, 'MMM DD, YYYY')}</time>
-//                <h1 class={styles.title}>{header.title || 'Untitled'}</h1>
-//                {content}
-//            </article>
-//        );
-//    }
-//}
+const styles = {
+    title: 'f2 lh-title',
+    date: 'f6',
+    p: 'f5 lh-copy',
+};
+
+function h(tag, attr, children) {
+    var style = styles[tag];
+
+    if (style) {
+        attr.className = style;
+    }
+
+    console.log(tag, attr, children);
+    return createElement(tag, attr, children);
+}
+
+function render(nodes, short) {
+    function e(n) { return Array.isArray(n) ? h(n[0], n[1], n.length < 3 ? undefined : n[2].map(e)) : n; }
+
+    if (short) {
+        nodes = nodes.slice(0, 6);
+    }
+
+    return nodes.map(e);
+}
 
 function Post({ stub, short }) {
-    return posts(stub).content(createElement);
+    var post = posts(stub);
+    var header = post.header;
+    var stubMatch = stub.match(/.\/((\d+-\d+-\d+)-[^.]+).*/);
+
+    var id = stubMatch[1];
+    var date = header.date || stubMatch[2];
+
+    return (
+            <article id={id}>
+            <time class={styles.date} datetime={date}>{format(date, 'MMM DD, YYYY')}</time>
+            <h1 class={styles.title}>{header.title || 'Untitled'}</h1>
+            {render(post.content, short)}
+        </article>
+    );
+    return render(posts(stub), short);
 }
 
 export default Post;
--- src/post-loader.js
+++ src/post-loader.js
@@ -1,4 +1,3 @@
-const stringify = require('rehype-stringify');
 const remark = require('remark');
 const highlight = require('remark-highlight.js');
 const frontmatter = require('remark-frontmatter');
@@ -7,16 +6,10 @@ const cleanRaw = require('hast-util-raw');
 const yaml = require('js-yaml');
 const toH = require('hast-to-hyperscript');
 
-const styles = {
-    title: 'f2 lh-title',
-    date: 'f6',
-    p: 'f5 lh-copy',
-};
-
 function lazy() {
     this.Compiler = compile;
 
-    function h(tag, attr, child) {
+    function h() {
         return Array.prototype.slice.call(arguments);
     }
 
@@ -26,13 +19,13 @@ function lazy() {
 
         var asHAST = toHAST(node, { allowDangerousHTML: true });
         var cleaned = cleanRaw(asHAST);
-        return { header: header, content: toH(h, cleaned)};
+
+        return { header: header, content: toH(h, cleaned)[2]};
     }
 }
 
 module.exports = function postLoader(source) {
     this.value = source;
-    // this.resourcePath;
 
     const post = remark()
         .use(highlight)
@@ -40,20 +33,9 @@ module.exports = function postLoader(source) {
         .use(lazy)
         .processSync(source).contents;
 
-    const header = JSON.stringify(post.header);
-    const content = JSON.stringify(post.content);
-
-    const val = {
-        header: post.header,
-        content: function(h) {
-            function e(n) { return Array.isArray(n) ? h(n[0], n[1], n.length < 3 ? undefined : n[2].map(e)) : n; }
-            return e(post.content);
-        }
-    };
-
-    const json = JSON.stringify(val)
+    const json = JSON.stringify(post)
           .replace(/\u2028/g, '\\u2028')
           .replace(/\u2029/g, '\\u2029');
 
-    return `module.exports = { header: ${header}, content: function(h) { function e(n) { return Array.isArray(n) ? h(n[0], n[1], n.length < 3 ? undefined : n[2].map(e)) : n; }; return e(${content})}}`;
+    return `module.exports = ${json};`;
 };
